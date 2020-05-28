427dec2d03ec7bf7795c39c2136d3e4edae79290
”dai03” <d.taniguchi0803@gmail.com>
2020-05-28 17:01:28 +0000

    rspec requests/comments add describe

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -44,4 +44,105 @@ RSpec.describe "Example", type: :request do
     
   end
   
+  
+  describe "create" do
+    
+    let!(:user) {FactoryBot.create(:user)}
+    let!(:other_user) {FactoryBot.create(:user)}
+    let!(:topic) {FactoryBot.create(:topic, user_id: other_user.id)}
+    
+    context "ログイン済みで有効な属性であるとき" do
+      
+        let!(:comment) {FactoryBot.build(:comment)}
+      
+      it "投稿にコメントできる" do
+        
+        sign_in user
+        
+        
+        expect do
+          post comments_path, params: {comment: comment }, params: {comment: {topic_id: topic.id}}
+        end.to change(Comment, :count).by(1)
+      end
+      
+      it "topics_pathにリダイレクトされる" do
+        
+        sign_in user
+        
+        post comments_path, params: {comment: comment }, params: {comment: {topic_id: topic.id}}
+        
+        expect(response).to redirect_to topics_path
+      end
+    end
+    
+    context "ログイン済みでないとき" do
+      
+      let!(:comment) {FactoryBot.build(:comment)}
+      
+      it "投稿にコメントできない" do
+        
+        expect do
+          post comments_path, params: {comment: comment }, params: {comment: {topic_id: topic.id}}
+        end.to_not change(Comment, :count)
+      end
+      
+      it "ログイン画面にリダイレクトされる" do
+        
+        post comments_path, params: {comment: comment }, params: {comment: {topic_id: topic.id}}
+        
+        expect(response).to redirect_to login_url
+      end
+    end
+    
+  end
+  
+  
+  describe "destroy" do
+    
+    let!(:user) {FactoryBot.create(:user)}
+    let!(:other_user) {FactoryBot.create(:user)}
+    let!(:topic) {FactoryBot.create(:topic, user_id: other_user.id)}
+    let!(:comment) {FactoryBot.create(:comment, user_id: user.id, topic_id: topic.id)}
+    
+    context "ログイン済みかつ現在のユーザーであるとき" do
+      
+      it "コメントを削除できる" do
+        
+        sign_in user
+        
+        expect do
+          delete comment_delete_path, params: {id: comment.id}
+        end.to change(Comment, :count).by(-1)
+      end
+      
+      it "topics_pathにリダイレクトされる" do
+        
+        sign_in user
+        
+        delete comment_delete_path, params: {id: comment.id}
+        
+        expect(response).to redirect_to topics_path
+      end
+    end
+    
+    context "ログイン済みでないとき" do
+      
+      it "コメントを削除できない" do
+        
+        expect do
+          delete comment_delete_path, params: {id: comment.id}
+        end.to_not change(Comment, :count)
+      end
+      
+      it "ログイン画面にリダイレクトされる" do
+        
+        delete comment_delete_path, params: {id: comment.id}
+        
+        expect(response).to redirect_to login_url
+      end
+    end
+    
+  end
+  
+  
 end
